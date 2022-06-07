adaeb40b3f9f4ff79121d00d0139545c2594d94e
ken <kchiem12@gmail.com>
2022-06-07 19:25:08 -0400

    Account for negative counter exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,7 @@
 const repeatString = function(word, amount) {
     counter = amount;
     if (counter == 0) return "";
+    else if (counter < 0) return 'ERROR';
 
     return word + repeatString(word, --counter);
 };
