d3968604a7a658e4b549fb59827c781d6479ef15
ken <kchiem12@gmail.com>
2022-06-08 17:18:09 -0400

    Implement Fibonacci sequence
    Only weird part is that in the recursive call, I had to decrement
    two different values (num and index), otherwise it would not work.
    Maybe it has to do with the parseInt() or some JavaScript rule

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    num = Number(index); 
 
+    if (num < 0) return 'OOPS';
+
+    if (num == 1 || num == 2) return 1;
+
+    return fibonacci(num - 1) + fibonacci(index - 2);
 };
 
 // Do not edit below this line
