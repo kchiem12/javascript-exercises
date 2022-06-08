a0e52175cf027e5b083c8fbba3c657ebad6a1319
ken <kchiem12@gmail.com>
2022-06-07 22:41:06 -0400

    Implement addition in the sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
 const sumAll = function(num1, num2) {
-    if (typeof num1 !== 'number' || typeof num2 !== 'number') return 'ERROR';
-    
+    if (typeof num1 !== 'number' || typeof num2 !== 'number' || num1 < 0 || num2 < 0) return 'ERROR';
+
+    start = Math.min(num1, num2);
+    dist = Math.abs(num1-num2);
+    result = 0;
+
+    for (let i = start; i <= (start+dist); i++) {
+        result = result + i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
