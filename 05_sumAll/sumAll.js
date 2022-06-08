c5ce53d3c8ab97dfdbda128276486f69f443a7d8
ken <kchiem12@gmail.com>
2022-06-07 22:28:15 -0400

    Check type of arguments in exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,6 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') return 'ERROR';
+    
 };
 
 // Do not edit below this line
