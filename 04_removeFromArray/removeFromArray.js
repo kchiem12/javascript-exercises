f825700ffd132a226e828dc174cfc56504011897
ken <kchiem12@gmail.com>
2022-06-07 22:23:20 -0400

    Correctly implement function in exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+/*
 
+can use splice() <-- to remove some parts from the array
+slice() <-- does not remove anything from the original array, but can bring elements to a new array
+at() <-- can use for indexing an array
+findIndex() <-- can be used to find a specified element
+...<-- used to specify an indefinite number of arguments
+
+maybe use the filter() method to filter out everything listed
+
+*/
+
+const removeFromArray = function(arr, ...theArgs) {
+    return arr.filter(element => !theArgs.includes(element));
 };
 
 // Do not edit below this line
