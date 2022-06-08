17e77099ca75492a64c35a56b0c3d147218d6df9
ken <kchiem12@gmail.com>
2022-06-08 13:44:47 -0400

    Correct palindrome function in exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    editString = string
+    editString = editString.toLowerCase().replace(/\s/g, '');
+    editString = editString.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, ''); //removes all punctuation
 
+    cleanString = editString;
+
+    editString = editString.split('');
+    temp = editString
+    for (let i = 0; i < editString.length; i++) {
+        editString[i] = temp[editString.length - 1 - i];
+    }
+
+    editString = editString.join('');
+    if (editString===cleanString) return true;
+    return false;
+    
 };
 
 // Do not edit below this line
