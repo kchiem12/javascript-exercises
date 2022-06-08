5925885d6e08391e07561115a239db4a4d4ca32f
ken <kchiem12@gmail.com>
2022-06-08 13:07:26 -0400

    Implement a calculator in exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  result = 0;
+	for (let i = 0; i < arr.length; i++) {
+    result = add(result, arr[i]);
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  result = 0;
+  arr.length == 0 ? result : result = 1;
+  for (let i = 0; i < arr.length; i++) {
+    result = result * arr[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0) return 1;
+  return num * factorial(--num);
 };
 
 // Do not edit below this line
