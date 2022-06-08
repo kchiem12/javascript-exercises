c721ace650c187b1eb19d7fa19ce80deea31a428
ken <kchiem12@gmail.com>
2022-06-08 12:45:15 -0400

    Implement leapYear function exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
+//divisible by 4, not 100, but 400 works
 
+const leapYears = function(year) {
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) return true;
+    return false;
 };
 
 // Do not edit below this line
